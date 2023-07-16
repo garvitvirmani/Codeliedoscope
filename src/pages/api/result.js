ade5e323370ccb4762cc568ddb5243857d3503ed
garvit-density <garvit.intern@density.exchange>
2023-07-16 21:56:00 +0530

    fix error

--- src/pages/api/result.js
+++ src/pages/api/result.js
@@ -1,54 +1,45 @@
 const puppeteer = require("puppeteer");
+const chrome = require("chrome-aws-lambda");
 const { NextApiRequest, NextApiResponse } = require("next");
 
 export default async function handler(req, res) {
   try {
-    // Launch a headless browser in the new Headless mode
-    const browser = await puppeteer.launch({ headless: "new" });
+    const browser = await puppeteer.launch({
+      executablePath: await chrome.executablePath,
+      args: chrome.args,
+      headless: chrome.headless,
+    });
 
-    // Open a new page
     const page = await browser.newPage();
 
-    // Navigate to the website
     await page.goto("https://www.shoppersstop.com/store-finder");
 
     console.log("started...");
 
-    // Wait for the necessary elements to load with a longer timeout (e.g., 120 seconds)
     await page.waitForSelector("#city-name", { timeout: 120000 });
 
-    // Get the city options
     const cityOptions = await page.$$eval("#city-name option", (options) =>
       options.map((option) => option.value)
     );
 
-    // Store the city and store information
     const cityStoreInfo = [];
 
-    // Iterate through each city option
     for (const cityOption of cityOptions) {
-      // Select a city
       await page.select("#city-name", cityOption);
 
-      // Wait for the store names to load
       await page.waitForSelector("#selectedPOS option", { timeout: 120000 });
 
-      // Get the store options
       const storeOptions = await page.$$eval("#selectedPOS option", (options) =>
         options.map((option) => option.value)
       );
 
-      // Store the store names and addresses under the current city
       const stores = [];
       for (let storeOption of storeOptions) {
         storeOption = storeOption.split(",");
-        // Select a store
         await page.select("#selectedPOS", storeOption.at(-1));
 
-        // Wait for the store address to load
         await page.waitForSelector(".store_address", { timeout: 120000 });
 
-        // Get the store name and address
         const storeName = await page.$eval(
           ".store_address h3",
           (element) => element.textContent
@@ -58,7 +49,6 @@ export default async function handler(req, res) {
           (element) => element.textContent
         );
 
-        // Store the store name and address in the array
         storeOption?.[0]?.length > 0 &&
           storeOption?.[1]?.length > 0 &&
           stores.push({
@@ -69,17 +59,14 @@ export default async function handler(req, res) {
           });
       }
 
-      // Store the city and stores in the array
       cityStoreInfo.push({
         city: cityOption,
         stores: stores,
       });
     }
 
-    // Close the browser
     await browser.close();
 
-    // Return the city and store information as the response
     res.status(200).json({ cityStoreInfo });
   } catch (error) {
     console.error("An error occurred:", error);
